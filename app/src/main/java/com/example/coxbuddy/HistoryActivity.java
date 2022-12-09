fc06fa9fa687c40f2c17b22f5cc94ef5d0ad26af
raphaelsanchez0 <sanchezraphael0@gmail.com>
2022-12-09 18:06:27 -0500

    Listview object created, not working yet.

--- app/src/main/java/com/example/coxbuddy/HistoryActivity.java
+++ app/src/main/java/com/example/coxbuddy/HistoryActivity.java
@@ -4,27 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
     public class HistoryActivity extends AppCompatActivity {
+        ListView sessionNameListView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-
-
-
-
+        sessionNameListView = (ListView) findViewById(R.id.history_ListVIew);
+        ArrayAdapter<String> arrayAdapter =  new ArrayAdapter<String>(this,R.layout.activity_history,R.id.history_ListVIew,getSessionsFromDir());
+        sessionNameListView.setAdapter(arrayAdapter);
     }
 
-    public void getSessionsFromDir(){
+    public List<String> getSessionsFromDir(){
+        List <String> sessionNames = new ArrayList<>();
         File[] files = getApplicationContext().getFilesDir().listFiles();
         for(int i=0;i<files.length;i++){
-
+            sessionNames.add(files[i].getName());
         }
+        return sessionNames;
     }
 
 }
